a4bcad68b9dbd0ad1373b5db98b771ca53482584
csPython <cspython@AD25TF04NGF1J-42.local>
2019-03-25 13:24:44 -0400

    custom circle function

--- bullsEyeNC.py
+++ bullsEyeNC.py
@@ -1,11 +1,16 @@
 from graphics import *
+from random import *
 
 def mk_circle(rad, cirX, cirY, color, win):
     bullRing = Circle(Point(cirX, cirY), rad)
     bullRing.setFill(color)
     bullRing.draw(win)
 
+rad = randint(10, 100)
+cirXY = randint(200, 400)
+color = color_rgb(randint(0, 255), randint(0, 255), randint(0, 255))
+
 win = GraphWin("BullsEye", 750, 750)
 win.setCoords(0, 0, 750, 750)
 
-mk_circle(50, 375, 375, "hot pink", win)
+mk_circle(rad, cirXY, cirXY, color, win)
